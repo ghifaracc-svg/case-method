46adf5c057787c7b72508ef5ad7137e8ae75a86d
Muhammad Abu Dzar Al Ghifari <ghifaracc@gmail.com>
2026-05-26 09:09:11 +0700

    2

---
+++ Order.java
@@ -0,0 +1,11 @@
+public class Order {
+    int orderCode;
+    String orderName;
+    int price;
+
+    Order(int orderCode, String orderName, int price) {
+        this.orderCode = orderCode;
+        this.orderName = orderName;
+        this.price = price;
+    }
+}
