ebd6321aaf08efc53b4ba3564997fbc58590bb2e
Muhammad Abu Dzar Al Ghifari <ghifaracc@gmail.com>
2026-05-26 09:08:42 +0700

    1

---
+++ Buyer.java
@@ -0,0 +1,11 @@
+public class Buyer {
+    int queueNumber;
+    String name;
+    String phoneNumber;
+
+    Buyer(int queueNumber, String name, String phoneNumber) {
+        this.queueNumber = queueNumber;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+}
